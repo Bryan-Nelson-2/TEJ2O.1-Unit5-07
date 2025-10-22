4175f774994ef8f81f058258b311b9957db07512
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-10-22 13:30:52 -0400

    Cleaned up the code

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bryan
+ * Created on: Oct 2025
+ * This program makes a servo turn when buttons a and b are pressed.
 */
 
-basic.showString('Hello, World!')
+
+// variable
+const servoNumber1 = robotbit.Servos.S1
+
+// set up
+basic.clearScreen()
+basic.showIcon (IconNames.Happy)
+
+// turns to 0 degrees when you press A
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString('0')
+    robotbit.Servo(servoNumber1, 0)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// turns to 180 degrees when you press B
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showString('180')
+    robotbit.Servo(servoNumber1, 180)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
